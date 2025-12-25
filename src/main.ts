c6a5b99133df64290a8d2e36ba3fde463858ddb4
ffeldhaus <florian.feldhaus@gmail.com>
2025-12-25 23:11:58 +0100

    chore: increment version to 1.0.7 and refine i18n fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import packageJson from '../package.json';
 (window as any).global = window;
-console.log(`Soil Version ${packageJson.version} (main.ts)`);
+console.log(`Soil Version ${packageJson.version} (main.ts - i18n fix v2)`);
 import { bootstrapApplication } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-registerLocaleData(localeDe);
+registerLocaleData(localeDe, 'de');
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
