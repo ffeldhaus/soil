719c0bc055c692b7f9d41c90b6f73ed59b66645d
ffeldhaus <florian.feldhaus@gmail.com>
2026-01-16 19:03:35 +0100

    chore(scripts): exclude .xlf files from size check

--- scripts/check-file-size.js
+++ scripts/check-file-size.js
@@ -5,7 +5,7 @@ const { execSync } = require('node:child_process');
 const warnSize = 128 * 1024; // 128KB
 const errorSize = 256 * 1024; // 256KB
 
-const exceptions = ['package-lock.json', 'dist/', '.angular/', 'coverage/'];
+const exceptions = ['package-lock.json', 'dist/', '.angular/', 'coverage/', '.xlf'];
 
 const includedExtensions = ['.ts', '.html', '.scss', '.css', '.js', '.md', '.json', '.xlf'];
 
